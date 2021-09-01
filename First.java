681325002e1e69cca5d6bdfcc62848885cd0a1c4
a1cd <71281043+a1cd@users.noreply.github.com>
2021-09-01 11:02:14 -0400

    Args Added

--- First.java
+++ First.java
@@ -1,5 +1,25 @@
+/*
+ ╭────────────────────────────────────────────────────╮
+ │                                                    ╵
+ │ File: First.java
+ │ Project: FirstProject
+ │
+ │ Created by Everett Wilber on 21/08/30 at 02:56 PM.
+ │                                                    ╷
+ ╰────────────────────────────────────────────────────╯
+ */
+
+import java.net.*;
+
 public class First {
+    public static void print(String str) {
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        print("Hello World!");
+        for (String word : args) {
+            print(word);
+        }
     }
 }
