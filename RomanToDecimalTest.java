e0959bc868425e0aedb14ed1f71f3ab731214add
a1cd <71281043+a1cd@users.noreply.github.com>
2021-10-29 10:39:29 -0400

    graph math and stuff

--- RomanToDecimalTest.java
+++ RomanToDecimalTest.java
@@ -22,13 +22,20 @@ import static org.junit.Assert.*;
  * @version 2021.10.03
  */
 public class RomanToDecimalTest {
+    /**
+     * test all different tests in one test
+     */
     @Test
     public void romanToDecimal() {
         Invalid();
         NormalAndInvariants();
         LogicallyIncorrect();
     }
-    @Test 
+
+    /**
+     * test NormalAndInvariants
+     */
+    @Test
     public void NormalAndInvariants() {
         assertEquals(259,RomanToDecimal.romanToDecimal("CCLIX"));
         assertEquals(4,RomanToDecimal.romanToDecimal("IV"));
@@ -39,7 +46,11 @@ public class RomanToDecimalTest {
         assertEquals(109,RomanToDecimal.romanToDecimal("CIX"));
         assertEquals(1090,RomanToDecimal.romanToDecimal("MXC"));
     }
-    @Test 
+
+    /**
+     * test logically incorrect
+     */
+    @Test
     public void LogicallyIncorrect() {
         assertNotEquals(47, RomanToDecimal.romanToDecimal("IVIVIVIXIXIX"));
         assertEquals(9,RomanToDecimal.romanToDecimal("IX"));
@@ -48,24 +59,40 @@ public class RomanToDecimalTest {
         assertEquals(8,RomanToDecimal.romanToDecimal("IIX")); // weird but still works
 
     }
-    @Test 
+
+    /**
+     * test invalid
+     */
+    @Test
     public void Invalid() {
         assertEquals(-1,RomanToDecimal.romanToDecimal("IC"));
         assertEquals(-1,RomanToDecimal.romanToDecimal("HAHA"));
         assertNotEquals(4,RomanToDecimal.romanToDecimal("BeBopBoop"));
         assertNotEquals(1000, RomanToDecimal.romanToDecimal("oops"));
     }
-    @Test 
+
+    /**
+     * test to see if psvm works
+     */
+    @Test
     public void TestMain() {
         String[] args = {"XVI", "ICSXXXS"};
         RomanToDecimal.main(args);
     }
-    @Test 
+
+    /**
+     * test a bunch of tests
+     */
+    @Test
     public void TestRTDTests() {
         String[] allTest = {"I", "i", "II", "ii", "III", "iii", "IV", "iv", "V", "v", "VI", "vi", "VII", "vii", "VIII", "viii", "IX", "ix", "X", "x", "XI", "xi", "XIV", "xiv", "XV", "xv", "XIX", "xix", "XXIX", "xxix", "XXXIV", "xxxiv", "XLIX", "xlix", "LXXXIV", "lxxxiv", "XCIV", "xciv", "XCIX", "xcix", "CCCXCIV", "cccxciv", "CDXCIV", "cdxciv", "CMXCIV", "cmxciv", "MCMLXII", "mcmlxii", "MMCDIX", "mmcdix", "MMCDXCIV", "mmcdxciv", "CMXCIV", "cmxciv", "CMXCIV", "cmxciv", "MMCMXCIX", "mmcmxcix", "MMMMCMXCIX", "mmmmcmxcix", "IIVI", "iivi", "IIIVII", "iiivii", "IIIXVIII", "iiixviii", "IXIXIX", "ixixix", "IVIXXLXCCDCM", "ivixxlxccdcm", "XXXXXXXXXX", "xxxxxxxxxx", "LXCID", "lxcid", "XMXVIVIIIIV", "xmxviviiiiv", "MXMXVIVIXII", "mxmxvivixii", "MXMXVIVIXIIVIII", "mxmxvivixiiviii", "MMCCCDLXIIIVIX", "mmcccdlxiiivix", "MMMCMCMCDIIIX", "mmmcmcmcdiiix", "MMMMMCDCDCD", "mmmmmcdcdcd", "I_am_Batman", "ISIS_is_not_a_valid_Roman_numeral", "MMMMMGood", "mmmmmgood", "MMCMLXIJMMCD", "mmcmlxijmmcd", "LXHID", "lxhid", "IVXLCDME", "ivxlcdme", "SWEET_BRUH", "sweet_bruh"};
         RomanToDecimal.main(allTest);
     }
 
+    /**
+     * make sure that incorrect inputs put into rtdthrows
+     * will cause a throw
+     */
     @SuppressWarnings("CodeBlock2Expr")
     @Test
     public void romanToDecimalThrows() {
@@ -73,6 +100,9 @@ public class RomanToDecimalTest {
         assertThrows(Exception.class, () -> {RomanToDecimal.romanToDecimalThrows("IM");}); // Should throw because it is a level difference > 2
     }
 
+    /**
+     * run tests that test main (psvm)
+     */
     @Test
     public void main() {
         TestMain();
